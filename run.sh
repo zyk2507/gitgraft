a5124677e4b2b0b1c0e8f87c65a2e01d5c06c89d
zyk2507 <93830642+zyk2507@users.noreply.github.com>
2022-09-26 10:53:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ sudo -E apt update
 sudo -E apt install git coreutils -y
 
 WORKDIR="$(pwd)"
-wget https://gist.githubusercontent.com/Victrid/58d13520f3c00d954656de1c8e777faf/raw/6ca4942592b2012b6ced660e06a8fce5c43c54f3/gitgraft.sh
 
 git config --global user.email "example@example.com"
 git config --gloabal user.name "example"
