5775adc053f3d95be650931538a04065eba2deca
zyk2507 <93830642+zyk2507@users.noreply.github.com>
2022-09-26 14:48:47 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 sudo -E apt update
 sudo -E apt install git coreutils diffstat -y
-wget https://github.com/zyk2507/gitgraft/blob/master/run.sh
+wget https://gist.githubusercontent.com/Victrid/58d13520f3c00d954656de1c8e777faf/raw/6ca4942592b2012b6ced660e06a8fce5c43c54f3/gitgraft.sh
 WORKDIR="$(pwd)"
 
 git config --global user.email "example@example.com"
